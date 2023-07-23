309e13a93de2feba9c9732d3b05e54facb86843e
MrAlute <abichioduye@gmail.com>
2023-07-23 03:12:27 +0100

    added a new folder

---
+++ simple_shell/_atoi.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * interactive - returns only true
+ * @info: address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if the character is a delimeter
+ * @c: the char
+ * @delim: the string
+ * Return: 1 true, 0 false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha - This function checks whether a character is an alphabetic character.
+ * @c: The character to be checked.
+ * Return: Returns 1 if 'c' is an alphabetic character, otherwise returns 0.
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - This function converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: Returns 0 if there are no numbers in the string, otherwise returns the converted number.
+*/
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
